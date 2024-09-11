0b608610111d8d16a7ace6f8349e8484ab7ad2d2
George Shaw <george.e.shaw.iv@outlook.com>
2024-09-10 22:35:33 -0700

    add all languages i want to treesitter

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -1,3 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "go", "gomod", "gosum", "gotmpl", "helm", "java", "javascript", "json", "jsonnet", "kotlin", "lua", "make", "markdown", "markdown_inline", "python", "ruby", "scss", "sql", "terraform", "tsx", "typescript", "vim", "vimdoc", "yaml" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+  },
 }
