51d510b7de0be8e9bb721f03c1a29c0986777b38
George Shaw <george.e.shaw.iv@outlook.com>
2024-09-11 17:47:31 -0700

    add gitcommit to treesitter parsers and set nvim as core.editor in gitconfig

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -6,7 +6,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "go", "gomod", "gosum", "gotmpl", "helm", "java", "javascript", "json", "jsonnet", "kotlin", "lua", "make", "markdown", "markdown_inline", "python", "ruby", "scss", "sql", "terraform", "tsx", "typescript", "vim", "vimdoc", "yaml" },
+          ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "gitcommit", "go", "gomod", "gosum", "gotmpl", "helm", "java", "javascript", "json", "jsonnet", "kotlin", "lua", "make", "markdown", "markdown_inline", "python", "ruby", "scss", "sql", "terraform", "tsx", "typescript", "vim", "vimdoc", "yaml" },
           sync_install = false,
           highlight = { enable = true },
           indent = { enable = true },  
